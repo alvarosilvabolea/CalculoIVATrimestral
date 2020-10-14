fdbc06ee1892da6c55760fea80299855bc2fe38f
alvarosilvabolea <71507502+alvarosilvabolea@users.noreply.github.com>
2020-10-14 19:36:59 +0200

    segundo intento

--- CalculoIVATrimestral/src/edu/gestion/ejercico1.java
+++ CalculoIVATrimestral/src/edu/gestion/ejercico1.java
@@ -12,12 +12,19 @@ public class ejercico1 {
 			
 			ArrayList <Producto> productos = new ArrayList<Producto>();
 			productos.add( new Producto(0.75, "Leche"));
+			
 			productos.add( new Producto(0.50, "Pan"));
+			
 			productos.add( new Producto(8.99, "Salmon"));
+			
 			productos.add( new Producto(2.95, "Aguacates"));
+			
 			productos.add( new Producto(1.20, "Friegasuelos"));
+			
 			productos.add( new Producto(0.56, "Cerveza"));
+			
 			productos.add( new Producto(1.45, "Sandia"));
+			
 			Factura facturas = new Factura(0, productos);
 			System.out.println(facturas.toString());
 	}
